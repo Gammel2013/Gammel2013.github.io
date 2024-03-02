890c9cf45dea3c66f9cdc40a6fd39e329459502f
Gammel2013 <160730477+Gammel2013@users.noreply.github.com>
2024-03-02 16:25:54 +0100

    Consistent naming, slight changes to js, display multiplier on button, add favicon

--- flask/static/game.js
+++ flask/static/game.js
@@ -1,16 +1,18 @@
 // JavaScript for the game logic
 
-let points = 0;
+const resources = {
+  points: 0,
+}
 
-let last_tick = performance.now();
-let elapsed_ms_this_tick = 0;
+let lastTick = performance.now();
+let elapsedMsEachTick = 0;
 
 
-function PGenerator(name, production, production_type, price, scaling) {
+function PGenerator(name, production, productionType, price, scaling) {
   this.count = 0;
   this.name = name;
   this.production = production;
-  this.production_type = production_type;
+  this.productionType = productionType;
   this.price = price;
   this.scaling = scaling;
 }
@@ -65,26 +67,38 @@ function init_window(){
   });
 
   // Misc stuff
-  updateGeneratorLabels();
-  updatePoints();
+  updateVisuals();
 }
 
 // Functions for updating labels
-function updatePoints() {
-  select('#points').textContent = points.toFixed(2);
-}
+function updateVisuals() {
+  // Update points
+  select('#points').textContent = resources.points.toFixed(2);
 
-function updateGeneratorLabels() {
+  // Update generator labels
   for (let generator of generatorList) {
     id = '#button_' + generator.name;
     select(id + '_amount').textContent = Math.trunc(generator.count);
     select(id + '_cost').textContent = generator.price.toFixed(2);
   }
+
+  // Update buttons for styling
+  for (let generator of generatorList) {
+    id = '#button_' + generator.name;
+    let el = select(id);
+    if (resources.points >= generator.price) {
+      el.classList.add('buyableGenerator');
+      el.classList.remove('notBuyableGenerator');
+    } else {
+      el.classList.remove('buyableGenerator');
+      el.classList.add('notBuyableGenerator');
+    }
+  }
 }
 
 // Functions for handling clicks
 function pointClick() {
-  points += 1; // Increase points by 1 on each click
+  resources.points += 1; // Increase points by 1 on each click
 }
 
 function handleTabClick() {
@@ -102,53 +116,39 @@ function handleTabClick() {
 function buyGenerator(event) {
   // get matching generator from button attributes
   let target_name = event.currentTarget.getAttribute('data-name');
-  let target_generator = getGenerator(target_name);
+  let targetGenerator = getGenerator(target_name);
   
-  if (points >= target_generator.price) {
-    points -= target_generator.price;
-    target_generator.price *= target_generator.scaling;
-    target_generator.count += 1;
+  if (resources.points >= targetGenerator.price) {
+    resources.points -= targetGenerator.price;
+    targetGenerator.price *= targetGenerator.scaling;
+    targetGenerator.count += 1;
   }
-  updateGeneratorLabels();
 }
 
 function productionTick() {
   for (let generator of generatorList) {
-    let production_this_tick = generator.count * generator.production * elapsed_ms_this_tick / 1000;
+    let productionThisTick = generator.count * generator.production * elapsedMsEachTick / 1000;
 
-    if (generator.production_type === 'points') {
-      points += production_this_tick;
-    } else if (generator.production_type.startsWith('pointGen')) {
-      let target_gen = getGenerator(generator.production_type);
-      target_gen.count += production_this_tick;
+    if (generator.productionType === 'points') {
+      resources.points += productionThisTick;
+    } else if (generator.productionType.startsWith('pointGen')) {
+      let targetGen = getGenerator(generator.productionType);
+      targetGen.count += productionThisTick;
     }
   }
 }
 
 function gameLoop() {
-  let start_of_tick = performance.now();
-  elapsed_ms_this_tick = start_of_tick - last_tick;
-  last_tick = start_of_tick
-  // update buttons for styling
-  for (let generator of generatorList) {
-    id = '#button_' + generator.name;
-    let el = select(id);
-    if (points >= generator.price) {
-      el.classList.add('buyableGenerator');
-      el.classList.remove('notBuyableGenerator');
-    } else {
-      el.classList.remove('buyableGenerator');
-      el.classList.add('notBuyableGenerator');
-    }
-  }
+  let startOfTick = performance.now();
+  elapsedMsEachTick = startOfTick - lastTick;
+  lastTick = startOfTick;
 
   productionTick();
 
-  updatePoints(); // Update points on the page
-  updateGeneratorLabels();
+  updateVisuals(); // Update points on the page
 
-  let end_of_tick = performance.now();
-  select('#elapsed_tick_time').textContent = end_of_tick - start_of_tick;
+  let endOfTick = performance.now();
+  select('#elapsed_tick_time').textContent = endOfTick - startOfTick;
 }
 
 // Logic when the game is loaded
